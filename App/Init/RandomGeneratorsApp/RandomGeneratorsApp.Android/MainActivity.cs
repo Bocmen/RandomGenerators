00f5a96eefa1352d3d57c7eef11371501cbc672e
Bocmen <60100681+Bocmen@users.noreply.github.com>
2023-05-04 16:39:26 +0300

    Added visual resources

--- App/Init/RandomGeneratorsApp/RandomGeneratorsApp.Android/MainActivity.cs
+++ App/Init/RandomGeneratorsApp/RandomGeneratorsApp.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace RandomGeneratorsApp.Droid
 {
-    [Activity(Label = "RandomGeneratorsApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "RandomGeneratorsApp", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
